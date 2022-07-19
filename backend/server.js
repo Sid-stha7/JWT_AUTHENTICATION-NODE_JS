8f77ec1eabd9757232ed074db27a5014838c7f57
sid_stha7 <sidstha444@gmail.com>
2022-07-19 15:52:13 +0545

    cutom error handler an async handler

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const dotenv = require ("dotenv").config()
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 5000
+const { errorHandler } = require("../middleware/errorMiddleware")
 
 const app = express()
 
@@ -9,6 +10,13 @@ const app = express()
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+/** since the error handler is middleware we have to use it as middleware so that will replace default 
+    express error handler
+**/
+app.use(errorHandler)
+
+
 //route
 app.use("/api/goals", require("../routes/goalRoutes"))
 app.listen(port,()=>{
--- controllers/goalController.js
+++ controllers/goalController.js
@@ -1,42 +1,46 @@
-
+/**since we are woking with error handles we wont use async await try catch 
+ * cuz we will be using  asyncHandler from express
+ */
+const asyncHandler= require("express-async-handler")
 
 //@desc Get goals
 //@route GET /api/goals
 // @access  Private
-const getGoal = (req, res)=>{
+const getGoal = asyncHandler(async (req, res)=>{
     res.status(200).json({message: "Get goals"})
 
-}
+})
 
 
 //@desc Set goals
 //@route Post /api/goals
 // @access  Private
-const setGoal = (req, res)=>{
+const setGoal = asyncHandler(async (req, res)=>{
     if (!req.body.text) {
         res.status(400)
         throw new Error ('please add a text in a field ')
     }
     
     res.status(200).json({message: "Create goals"})
-}
+    console.log(req.body);
+})
 
 
 //@desc Update goals
 //@route PUT /api/goals/:id
 // @access  Private
-const updateGoal = (req, res)=>{
+const updateGoal = asyncHandler(async (req, res)=>{
     res.status(200).json({message: `Update goals ${req.params.id}`})
 
-}
+})
 
 
 //@desc Delete goals
 //@route DELETE /api/goals/:id
 // @access  Private
-const deleteGoal = (req, res)=>{
+const deleteGoal =asyncHandler( async (req, res)=>{
     res.status(200).json({message: `Delete goals ${req.params.id}`})
-}
+})
 
 module.exports = {
 
@@ -44,4 +48,4 @@ module.exports = {
     setGoal,
     updateGoal,
     deleteGoal,
-}
+} 
