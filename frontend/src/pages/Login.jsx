43bb038a7a73524f6b872972462cb0561d1f6e71
sid_stha7 <sidstha444@gmail.com>
2022-07-27 09:36:02 +0545

    redux login and registration

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,71 +1,106 @@
-import {useState, useEffect} from 'react'
-import { FaSignInAlt }  from 'react-icons/fa'
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+import { useSelector, useDispatch } from "react-redux";
+import { Navigate, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { login, reset } from "../features/auth/authSlice";
+import Spinner from "../components/Spinner";
 
 function Login() {
   /**since we have to rerender the form time and again use gonna use
-   * useState what use state basically does is it re renders the react component 
-   * while we input the text in form field it will not update because there is no state change 
-   * the initial state of form has all blank in it  because the react rendered the component only once 
-   * and since there is no state change it won't get re-rendered, the **/ 
-const [formData, setFormData] = useState({
-  email : '',
-  password: '',
- 
-})
-const {email, password} = formData //destruct the data
-const onChange =(e)=>{
- setFormData ((prevState)=>({
-    ...prevState,
-    [e.target.name]: e.target.value
- })) 
-} 
+   * useState what use state basically does is it re renders the react component
+   * while we input the text in form field it will not update because there is no state change
+   * the initial state of form has all blank in it  because the react rendered the component only once
+   * and since there is no state change it won't get re-rendered, the **/
 
-const onSubmit=(e)=>{
- e.preventDefault() //prevents from its default action like no submiting form when click submit
-}
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = formData; //destruct the data
+
+  //redux hooks
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const { user, isLoading, isError, isSuccess, message } = useSelector(
+    (state) => state.auth
+  );
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+
+    if (isSuccess || user) {
+      navigate("/");
+    }
+    dispatch(reset());
+  }, [user, message, isError, isSuccess, navigate, dispatch]);
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  if (isLoading) {
+    <Spinner />;
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault(); //prevents from its default action like no submiting form when click submit
+    const userData = {
+      email,
+      password,
+    };
+    dispatch(login(userData));
+  };
 
   return (
     <>
-    <section className='heading'>
-      <h1>
-        <FaSignInAlt/>Login 
-      </h1>
-      <p>Please Create an account </p>
-    </section>
-    
-    <section>
-      <form onSubmit={onSubmit}>
- 
-        <div className="form-group">
-        <input 
-          type="email"  
-          className='form-control' 
-          id='email'  
-          value={email} 
-          placeholder="Enter your email "
-          onChange={onChange}
-        />
-        </div>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Login and start setting goals</p>
+      </section>
 
-        <div className="form-group">
-        <input 
-          type="password"  
-          className='form-control' 
-          id='password'  
-          value={password} 
-          placeholder="Enter your password "
-          onChange={onChange}
-        />
-        </div>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              value={email}
+              placeholder="Enter your email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              value={password}
+              placeholder="Enter password"
+              onChange={onChange}
+            />
+          </div>
 
-
-        <div className="forom-group">
-          <button type='submit' className='btn btn-block'>Submit</button>
-        </div>
-      </form>
-    </section>
+          <div className="form-group">
+            <button type="submit" className="btn btn-block">
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
     </>
-  )
+  );
 }
 
-export default Login
+export default Login;
