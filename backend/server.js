2cf31db9c831da4150fca2f3f9912d409ed29d64
sid_stha7 <sidstha444@gmail.com>
2022-07-28 12:46:04 +0545

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,18 @@
-const express = require("express")
-const dotenv = require ("dotenv").config()
-const bodyParser = require('body-parser');
-const port = process.env.PORT || 5000
-const { errorHandler } = require("../middleware/errorMiddleware")
-const colors = require("colors")
-const connectDB = require("./config/db")
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 5000;
+const { errorHandler } = require("../middleware/errorMiddleware");
+const colors = require("colors");
+const connectDB = require("./config/db");
 
+connectDB();
+const app = express();
 
-connectDB()
-const app = express()
+//DB connection
 
-
-//DB connection 
-
-
-//middleware 
+//middleware
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,14 +20,24 @@ app.use(bodyParser.urlencoded({ extended: true }));
 /** since the error handler is middleware we have to use it as middleware so that will replace default 
     express error handler
 **/
-app.use(errorHandler)
-
+app.use(errorHandler);
 
 //route
-app.use("/api/goals", require("../routes/goalRoutes"))
-app.use("/api/users", require("../routes/userRoutes"))
+app.use("/api/goals", require("../routes/goalRoutes"));
+app.use("/api/users", require("../routes/userRoutes"));
 
-app.listen(port,()=>{
+//Server frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
 
-    console.log(`server started on ${port}`);
-})
+app.listen(port, () => {
+  console.log(`server started on ${port}`);
+});
